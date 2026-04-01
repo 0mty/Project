42d322f21a4c6ef06995a7180f5643bba08d6d02
0mty <tubshinbayrbayanmonkh@gmail.com>
2026-04-01 09:25:16 +0800

    fix import path

--- my-project/src/components/Web1.jsx
+++ my-project/src/components/Web1.jsx
@@ -5,8 +5,8 @@ import Features from "./Feature";
 import Schedule from "./Schedule";
 import Monitor from "./Monitor";
 import Service from "./Service";
-import Price from "./price";
-import Footer from "./footer";
+import Price from "./Price";
+import Footer from "./Footer";
 
 const Web1 = () => {
   return (
